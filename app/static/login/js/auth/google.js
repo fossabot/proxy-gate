c83383dce40a456d03c47bd1e6d5e4d8a248ff24
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2023-08-11 07:59:59 -0400

    Revert "attempt 1 for google auth"
    This reverts commit 6cb4c0f9c54dec75e8625c46b6cec46731304723.

--- app/static/login/js/auth/google.js
+++ app/static/login/js/auth/google.js
@@ -1,55 +1,5 @@
-// reference: https://developers.google.com/identity/gsi/web/guides/overview
-import utils from "../utils.js";
+const google = {};
 
-// we call the namespace gsi to avoid name collisions with google namespace
-const gsi = {};
+google.workflowStepStart = function workflowStepStart(buttonId) {};
 
-gsi.workflowSetup = function workflowSetup() {
-  const googleAuthMetaz = utils.proxyGateMetaz["google_auth"];
-  if (!googleAuthMetaz) {
-    utils.showError(new Error("No Google Auth Metaz provided."));
-    console.error("No Google Auth Metaz provided.");
-    return;
-  }
-
-  const googleClientId = googleAuthMetaz["client_id"];
-  console.log("Google Client ID: " + googleClientId);
-
-  if (!googleClientId) {
-    utils.showError(new Error("No Google Client ID specified."));
-    console.error("No Google Client ID specified.");
-    return;
-  }
-
-  window.onload = function () {
-    google.accounts.id.initialize({
-      client_id: googleClientId,
-      auto_select: true,
-      callback: gsi.workflowStepCallback,
-    //   context: "signin",
-      ux_mode: "redirect",
-    });
-  };
-};
-
-gsi.workflowStepStart = function workflowStepStart(buttonId) {
-  const urlSearchParams = new URLSearchParams(window.location.search);
-  const authWithGoogleButton = document.getElementById(buttonId);
-  if (!authWithGoogleButton) {
-    return;
-  }
-  google.accounts.id.prompt((notification) => {
-    console.log(notification);
-    if (notification.isNotDisplayed() || notification.isSkippedMoment()) {
-        document.cookie =  `g_state=;path=/;expires=Thu, 01 Jan 1970 00:00:01 GMT`;
-        gsi.workflowStepStart(buttonId);
-    }
-    
-  });
-};
-
-gsi.workflowStepCallback = function workflowStepCallback(CredentialResponse) {
-    console.log(CredentialResponse)
-  };
-
-export default gsi;
+export default google;
