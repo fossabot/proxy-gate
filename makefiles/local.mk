ad553d368c6dcdbd858d41853e053e5116bf2eb7
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2023-08-15 23:27:33 -0400

    Add ngrok auth token check and don't force create all containers

--- makefiles/local.mk
+++ makefiles/local.mk
@@ -1,8 +1,11 @@
 .PHONY : local-run
 
 local-run: build
+ifndef NGROK_AUTHTOKEN
+	$(error NGROK_AUTHTOKEN is not defined. Please set it before running this command.)
+endif
 	@echo "Running the Docker compose up..."
-	docker compose --file examples/docker-compose.yaml up --detach --force-recreate
+	docker compose --file examples/docker-compose.yaml up --detach
 
 docker-clean:
 	@echo "Running docker clean..."
