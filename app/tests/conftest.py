98c3685e7edaa6362f07081d81bbfb2df3751f41
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2023-08-20 23:46:16 -0400

    clean up conftest.py

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,8 +1,9 @@
-import pytest
-from app import init_app
-import tempfile
 import os
-from pathlib import Path
+import tempfile
+
+import pytest
+
+from app import init_app
 
 
 # Fixture to create and return the Flask app instance
@@ -14,19 +15,11 @@ def app():
     yield app
 
 
-# Fixture to create and return a test client for the app
-@pytest.fixture
-def client(app):
-    return app.test_client()
-
-
 def set_test_environment():
     # Create a temporary directory for data files
     os.environ["PROXY_GATE_DATA_DIR"] = tempfile.mkdtemp()
     os.environ["PROXY_GATE_CONFIG_DIR"] = os.path.join(tempfile.mkdtemp(), "configs")
     os.environ["PROXY_GATE_CONFIG_DIR"] = os.path.join(tempfile.mkdtemp(), "migrations")
-    os.environ["PROXY_GATE_SECRET_KEY_VALIDITY"] = "72"
-    os.environ["PROXY_GATE_SECRET_KEY_INTERIM_VALIDITY"] = "168"
     os.environ[
         "FLASK_SQLALCHEMY_DATABASE_URI"
     ] = f'sqlite:///{os.environ["PROXY_GATE_DATA_DIR"]}/app.db'
