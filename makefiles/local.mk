876be27b1298de76deb0b19a467f6f0738b99d12
Komail Kanjee <1796298+KomailKanjee@users.noreply.github.com>
2023-08-22 17:47:08 -0400

    Drop ngrok

--- makefiles/local.mk
+++ makefiles/local.mk
@@ -1,9 +1,6 @@
 .PHONY : local-run
 
 local-run: build
-ifndef NGROK_AUTHTOKEN
-	$(error NGROK_AUTHTOKEN is not defined. Please set it before running this command.)
-endif
 	@echo "Running the Docker compose up..."
 	docker compose --file examples/docker-compose.yaml up --detach
 
